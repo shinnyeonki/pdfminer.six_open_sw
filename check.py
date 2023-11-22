c93ad9353dba78250a0eeb51103364d3b17737c0
shinnk <sygys10293@gmail.com>
2023-11-22 14:06:17 +0900

    word 기능 추가

--- check.py
+++ check.py
@@ -1,6 +1,6 @@
 from pdfminer import word
 
 
-pdf_file = '/Users/shinnk/source/pdfminer.six_open_sw/samples/class/linux-2.pdf'
-doc_file = '/Users/shinnk/source/pdfminer.six_open_sw/test.docx'
+pdf_file = 'samples/class/linux-2.pdf' # 바꾸고 싶은 pdf 파일 경로
+doc_file = 'test.docx' # 바꿀 파일 이름 및 경로 따로 지정하지 않아도 동작함
 word.pdfToDocx(pdf_file, doc_file)
--- pdfminer/word.py
+++ pdfminer/word.py
@@ -1,17 +1,17 @@
-import pdf2docx
 from pdf2docx import Converter
 
-def pdfToDocx(pdf_file, docx_file):
+def pdfToDocx(pdf_file, docx_file = None):
+    # 작업 폴더 위치 변경
+    import os
+    os.chdir('/Users/shinnk/source/pdfminer.six_open_sw')
+    # python 경로 절대 경로로 변경
+    pdf_file = os.path.abspath(pdf_file)
+    # docx 파일이 없는 경우
+    if docx_file is None:
+        docx_file = pdf_file.replace('.pdf', '.docx')
     print('Converting {} to {}'.format(pdf_file, docx_file))
     cv = Converter(pdf_file)
     cv.convert(docx_file)
     cv.close()
 
 
-# pdf_file = '/Users/shinnk/source/pdfminer.six_open_sw/linux-3'
-# docx_file = '/Users/shinnk/source/pdfminer.six_open_sw/test.docx'
-# pdfToDocx(pdf_file, docx_file)
-
-
-# import os
-# print(os.path.dirname(__file__))
